9a7d4a9a741b937c3b8dd1f8abc2ae9f9e057bf0
Gian <Gian@Gi>
2016-09-18 14:19:04 +0800

    Added some packages and classes

---
+++ src/java/servlets/search/SearchAgeGroup.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets.search;
+
+import DAO.recordDAO;
+import Model.record;
+import Servlets.BaseServlet;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import static java.util.logging.Level.SEVERE;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static java.lang.String.valueOf;
+import static java.util.logging.Logger.getLogger;
+
+/**
+ *
+ * @author Shermaine
+ */
+public class SearchAgeGroup extends BaseServlet {
+
+        /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void servletAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+            String year = request.getParameter("query");
+        
+            recordDAO recordDAO = new recordDAO();
+            ArrayList<record> recordsAge = new ArrayList<>();
+            try {
+                recordsAge = recordDAO.SearchYear(200000000, 299999999, year);
+            } catch (ParseException ex) {
+                getLogger(SearchAgeGroup.class.getName()).log(SEVERE, null, ex);
+            }
+  
+            ArrayList<String> censusYear = new ArrayList<>();
+
+            for (int i = 0; i < recordsAge.size(); i++) {
+                    censusYear.add(valueOf(recordsAge.get(i).getCensusYear()));
+                }
+            Gson gson = new Gson();
+            String json = gson.toJson(censusYear);
+            response.getWriter().write("{\"suggestions\":" + json + "}");
+        }
+
+    }
+
