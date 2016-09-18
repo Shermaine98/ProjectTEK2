dae6a9cdaed7a58489edc8ddf0b0f2ed01acced6
Gian <Gian@Gi>
2016-09-18 21:21:55 +0800

    Added some packages and classes & Fixed Servlets :)

---
+++ srcGuest/guest/model/Topic.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guest.model;
+
+import java.util.ArrayList;
+import java.sql.Date;
+
+/**
+ *
+ * @author Geraldine
+ */
+public class Topic {
+    
+    private String title;
+    private String body;
+    private ArrayList<String> tags;
+    private ArrayList<Comments> commentsArray;
+    private String username;
+    private int userID;
+    private Date datePosted;
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the commentsArray
+     */
+    public ArrayList<Comments> getCommentsArray() {
+        return commentsArray;
+    }
+
+    /**
+     * @param commentsArray the commentsArray to set
+     */
+    public void setCommentsArray(ArrayList<Comments> commentsArray) {
+        this.commentsArray = commentsArray;
+    }
+
+    /**
+     * @return the datePosted
+     */
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    /**
+     * @param datePosted the datePosted to set
+     */
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    /**
+     * @return the body
+     */
+    public String getBody() {
+        return body;
+    }
+
+    /**
+     * @param body the body to set
+     */
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    /**
+     * @return the tags
+     */
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    /**
+     * @param tags the tags to set
+     */
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the userID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    
+    
+}
